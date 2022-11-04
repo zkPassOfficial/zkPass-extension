35cbba3c428d918210d1a4c45695e44085671b04
lin <lilinyudlmu@126.com>
2022-11-04 16:45:41 +0800

    buffer test

---
+++ src/utils/buffer.test.ts
@@ -0,0 +1,64 @@
+import { describe, expect, test } from '@jest/globals'
+import Buffer from './buffer'
+
+describe('numeric',()=>{
+
+  test('buffer.writeUint8(a)=buffer.readUint8()',()=>{
+    const buffer = new Buffer(128)
+    const a = 0x12
+    buffer.writeUint8(a)
+    expect(a).toBe(buffer.readUint8())
+    expect(buffer.offset).toBe(1)
+  })
+  
+  test('buffer.writeUint16(a)=buffer.readUint16()',()=>{
+    const buffer = new Buffer(128)
+    const a = 0x1234
+    buffer.writeUint16(a)
+    expect(a).toBe(buffer.readUint16())
+    expect(buffer.offset).toBe(2)
+  })
+  
+  test('buffer.writeUint24(b)=buffer.readUint24()',()=>{
+    const buffer = new Buffer(128)
+    const a = 0x123456
+    buffer.writeUint24(a)
+    expect(a).toBe(buffer.readUint24())
+    expect(buffer.offset).toBe(3)
+  })
+
+  test('buffer.writeUint32(b)=buffer.readUint32()',()=>{
+    const buffer = new Buffer(128)
+    const a = 0x12345678
+    buffer.writeUint32(a)
+    expect(a).toBe(buffer.readUint32())
+    expect(buffer.offset).toBe(4)
+  })
+
+  test('buffer.writeBytes(bytes)=buffer.readBytes(bytes.length)',()=>{
+    const buffer = new Buffer(128)
+    const bytes = new Uint8Array([ 0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99 ])
+    buffer.writeBytes(bytes)
+    expect(bytes).toEqual(buffer.readBytes(9))
+    expect(buffer.offset).toBe(9)
+  })
+
+  test('buffer.writeUint32(a)=buffer.readBytes(4)',()=>{
+    const buffer = new Buffer(128)
+    const a = 0x12345678
+    buffer.writeUint32(a)
+    expect(new Uint8Array([ 0x12,0x34,0x56,0x78 ])).toEqual(buffer.readBytes(4))
+  })
+
+  test('grow',()=>{
+    const buffer = new Buffer(128)
+
+    for (let i = 0; i < 130; i++) {
+      buffer.writeUint8(i)
+    }
+    
+    expect(buffer.offset).toBe(130)
+    expect(buffer.bytes.length).toBe(256)
+  })
+
+})
